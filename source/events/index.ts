3479c8d2ec74d4ebdeb87a403d7034f88f8eedf0
Selçuk Çukur <selcukcukur@outlook.com.tr>
2024-07-16 18:31:04 +0300

    `Events` class has been created.

---
+++ source/events/index.ts
@@ -0,0 +1,13 @@
+import { EventsInterface } from "./interface";
+
+/**
+ * @name         Events - SUI
+ * @description  <b>Events</b> - Next generation user and application interface kit for developers.
+ * @version      v1.0.0
+ * @author       Selçuk Çukur <hk@selcukcukur.com.tr>
+ * @license      The MIT License (https://docs.srylius.com/ui/license)
+ * @copyright    (C) 2010 - 2024 All rights reserved by Srylius.
+ **/
+class Events implements EventsInterface {
+    // ..
+}
