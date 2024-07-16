a4b118789ae493ed5aed1d1eab7c870b9c233ea0
Selçuk Çukur <selcukcukur@outlook.com.tr>
2024-07-16 22:52:39 +0300

    `InstanceOptions` type declaration for component instances.

---
+++ source/container/types.ts
@@ -0,0 +1,23 @@
+/**
+ * @name         InstanceOptions - SUI
+ * @description  <b>InstanceOptions</b> - Next generation user and application interface kit for developers.
+ * @version      v1.0.0
+ * @author       Selçuk Çukur <hk@selcukcukur.com.tr>
+ * @license      The MIT License (https://docs.srylius.com/ui/license)
+ * @copyright    (C) 2010 - 2024 Srylius (Srylius Teknoloji Limited Şirketi)
+ **/
+export declare type InstanceOptions = {
+    /**
+     * The instance identifier.
+     *
+     * @type {string}
+     */
+    id?: string;
+
+    /**
+     * The override status.
+     *
+     * @type {boolean}
+     */
+    override?: boolean;
+};
