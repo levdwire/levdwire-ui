d9f10034a94f69efd2bb261fa6673925738f5721
Selçuk Çukur <selcukcukur@outlook.com.tr>
2024-07-16 18:36:12 +0300

    `Component` class has been created.

---
+++ source/components/component/index.ts
@@ -0,0 +1,13 @@
+import { ComponentInterface } from "./interface";
+
+/**
+ * @name         Component - SUI
+ * @description  <b>Component</b> - Next generation user and application interface kit for developers.
+ * @version      v1.0.0
+ * @author       Selçuk Çukur <hk@selcukcukur.com.tr>
+ * @license      The MIT License (https://docs.srylius.com/ui/license)
+ * @copyright    (C) 2010 - 2024 Srylius (Srylius Teknoloji Limited Şirketi)
+ **/
+class Component implements ComponentInterface {
+    // ..
+}
