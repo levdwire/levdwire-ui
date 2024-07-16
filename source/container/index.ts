5429f403396e38ccb6eeb7d34dcbb9754f6e5a7d
Selçuk Çukur <selcukcukur@outlook.com.tr>
2024-07-16 18:28:23 +0300

    `Container` class has been created.

---
+++ source/container/index.ts
@@ -0,0 +1,13 @@
+import { ContainerInterface } from "./interface";
+
+/**
+ * @name         Container - SUI
+ * @description  <b>Container</b> - Next generation user and application interface kit for developers.
+ * @version      v1.0.0
+ * @author       Selçuk Çukur <hk@selcukcukur.com.tr>
+ * @license      The MIT License (https://docs.srylius.com/ui/license)
+ * @copyright    (C) 2010 - 2024 All rights reserved by Srylius.
+ **/
+class Container implements ContainerInterface {
+    // ..
+}
